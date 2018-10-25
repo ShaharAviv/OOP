f9f56d33cd9980ae02e1a46d408cdb9eaa2dab5d
Shahar Aviv <saviv.mail@gmail.com>
2018-10-25 16:10:53 +0300

    change k

--- ex1/ex1/Utilities.h
+++ ex1/ex1/Utilities.h
@@ -5,5 +5,5 @@ bool doubleEqual(double a, double b);
 
 double distance(const Vertex& v1, const Vertex& v2);
 bool sameX(const Vertex& v1, const Vertex& v2);
-bool sameY(const Vertex& v1, const Vertex& v2);
+bool sameY(const Vertex& v1, const Vertex& v2);k
 bool operator<(const Vertex& lhs, const Vertex& rhs);
